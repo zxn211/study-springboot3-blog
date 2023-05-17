dec7bd9be62c58a0e93e7548651aefd6a5d54591
zxn <834224861@qq.com>
2023-05-18 06:08:59 +0800

    初始化项目后端

---
+++ src/main/java/com/zxn/blog/ProjectBackedApplication.java
@@ -0,0 +1,13 @@
+package com.zxn.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBackedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zxn/blog/ProjectBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxn.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
